566a6093ea3039e89f94cbcae5b0e8b4240e8744
gushags <jeff@hagkull.com>
2025-08-11 12:27:37 -0700

    style: add styling to home page

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -7,9 +7,15 @@ function Home() {
   return (
     <>
       <Navigation />
-      <div className={styles.homepageBackground}>
-        <div>Background Image</div>
-        <div>Selling text</div>
+      <div className={styles.homepageBackground}></div>
+      <div className={styles.homeContainer}>
+        <h1>Welcome to theBIGstore!</h1>
+        <h4 className={styles.sellText}>
+          Paullum deliquit, ponderibus modulisque suis ratio utitur. Nec
+          dubitamus multa iter quae et nos invenerat. Lorem ipsum dolor sit
+          amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut
+          labore et dolore magna aliqua.
+        </h4>
         <button>Shop Now</button>
       </div>
     </>
