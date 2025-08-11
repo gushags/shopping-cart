fa760b472c7c92d38e8385390d2bfd08e8a8a9ca
gushags <jeff@hagkull.com>
2025-08-11 10:49:02 -0700

    style: begin styling navigation

--- src/navigation/Navigation.jsx
+++ src/navigation/Navigation.jsx
@@ -6,16 +6,21 @@ function Navigation() {
   return (
     <>
       <nav>
-        <Link to='/'>daBIGstore</Link>
-
+        <Link to='/' className={styles.logo}>
+          daBIGstore
+        </Link>
         <div>
-          <Link to='/shop'>Shop</Link>
+          <Link to='/' className={styles.pad}>
+            Home
+          </Link>
+          <Link to='/shop' className={styles.pad}>
+            Shop
+          </Link>
           <Link to='/cart'>
             Cart <span>0</span>
           </Link>
         </div>
       </nav>
-      <div className={styles.redText}>Navigation here</div>
     </>
   );
 }
