f49e7d056b25eb090e4d89855c14838fc6aa4553
gushags <jeff@hagkull.com>
2025-08-11 22:39:10 -0700

    feat: add table to cart page

--- src/cart/Cart.jsx
+++ src/cart/Cart.jsx
@@ -4,17 +4,106 @@ import Navigation from '../navigation/Navigation';
 import Item from '../item/Item';
 import styles from './Cart.module.css';
 
-function Cart() {
+export default function Cart() {
   return (
     <>
       <Navigation />
-      <div className={styles.cartContainer}>
-        <Item />
-        <Item />
-        <Item />
-      </div>
+      <table
+        border='0'
+        cellspacing='0'
+        cellpadding='0'
+        className={styles.cartContainer}
+      >
+        <tr className={styles.bottomBorder}>
+          <th>Product</th>
+          <th>Product Name</th>
+          <th>Quantity</th>
+          <th>Price</th>
+        </tr>
+        <Item
+          id={PRODUCTS[0].id}
+          image={PRODUCTS[0].image}
+          title={PRODUCTS[0].title}
+          price={PRODUCTS[0].price}
+        />
+        <Item
+          id={PRODUCTS[0].id}
+          image={PRODUCTS[0].image}
+          title={PRODUCTS[0].title}
+          price={PRODUCTS[0].price}
+        />
+      </table>
     </>
   );
 }
 
-export default Cart;
+// dummy data
+
+const PRODUCTS = [
+  {
+    id: 0,
+    category: "men's clothing",
+    description:
+      'Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.',
+    image:
+      'https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_t.png',
+    price: 22.3,
+    rating: { rate: 4.1, count: 259 },
+    title: 'Mens Casual Premium Slim Fit T-Shirts ',
+  },
+  {
+    id: 1,
+    category: "men's clothing",
+    description:
+      'great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.',
+    image:
+      'https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_t.png',
+    price: 55.99,
+    rating: { rate: 4.7, count: 500 },
+    title: 'Mens Cotton Jacket',
+  },
+  {
+    id: 2,
+    category: "men's clothing",
+    description:
+      'Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.',
+    image:
+      'https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_t.png',
+    price: 12.3,
+    rating: { rate: 4.1, count: 259 },
+    title: 'Mens Premium Slim Fit T-Shirts ',
+  },
+  {
+    id: 3,
+    category: "men's clothing",
+    description:
+      'great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.',
+    image:
+      'https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_t.png',
+    price: 95.99,
+    rating: { rate: 4.7, count: 500 },
+    title: 'Mens Cotton Jacket',
+  },
+  {
+    id: 4,
+    category: "men's clothing",
+    description:
+      'Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.',
+    image:
+      'https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_t.png',
+    price: 52.3,
+    rating: { rate: 4.9, count: 259 },
+    title: 'Mens Casual Premium Fit T-Shirts ',
+  },
+  {
+    id: 5,
+    category: "men's clothing",
+    description:
+      'great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.',
+    image:
+      'https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_t.png',
+    price: 90.99,
+    rating: { rate: 2.3, count: 500 },
+    title: 'Mens Heavy Cotton Jacket',
+  },
+];
--- src/item/Item.jsx
+++ src/item/Item.jsx
@@ -1,18 +1,24 @@
 // Item.jsx
 import styles from './Item.module.css';
 
-export default function Item({ id, image, title, quantity, price }) {
-  const subtotal = (Math.round(price * quantity * 100) / 100).toFixed(2);
-
+export default function Item({ id, image, title, quantity = 1, price }) {
   return (
     <>
-      <div key={id} className={styles.ItemContainer}>
-        <img src={image} alt={title} />
+      {/* <div key={id} className={styles.itemContainer}>
+        <img className={styles.image} src={image} alt={title} />
         <h4>{title}</h4>
         <p>{quantity}</p>
-        <p>{price}</p>
+        <p>{(Math.round(price * 100) / 100).toFixed(2)}</p>
         <h4>${subtotal}</h4>
-      </div>
+      </div> */}
+      <tr key={id}>
+        <td className={styles.imageTD}>
+          <img className={styles.image} src={image} alt={title} />
+        </td>
+        <td>{title}</td>
+        <td>{quantity}</td>
+        <td>${(Math.round(price * quantity * 100) / 100).toFixed(2)}</td>
+      </tr>
     </>
   );
 }
