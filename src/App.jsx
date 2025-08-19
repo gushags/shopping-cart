ac23ac4c1d1491a8ece5b5c5b98550bc272367e0
gushags <jeff@hagkull.com>
2025-08-18 20:05:14 -0700

    fix: fix typo in app file

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 
 import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import AppLayout from './applayout/Applayout';
+import AppLayout from './applayout/AppLayout';
 import Home from './home/Home';
 import Shop from './Shop/Shop';
 import Cart from './cart/Cart';
